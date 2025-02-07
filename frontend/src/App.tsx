c4731b3bc1f16a1e9add81f42a8de17453908c6b
Damjan Savkovic <damjans92@gmail.com>
2025-02-07 15:00:39 +0100

    first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+import { v4 as uuidv4 } from "uuid";
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl text-red">Color Manager</h1>
+    </>
+  );
+}
+
+export default App;
