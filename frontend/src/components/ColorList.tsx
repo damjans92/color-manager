3ce3d30554966748ac647a0cc5f0c6325ce7a9e3
Damjan Savkovic <damjans92@gmail.com>
2025-02-10 03:25:55 +0100

    added usememo for filteredColors, added Loader component

--- frontend/src/components/ColorList.tsx
+++ frontend/src/components/ColorList.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import ColorItem from "./ColorItem";
 import ColorModal from "./ColorModal";
 import { Color } from "../types";
@@ -7,6 +7,7 @@ import { AppDispatch, RootState } from "../redux/store";
 import { motion } from "framer-motion";
 import { toast } from "react-toastify";
 import { deleteColor, fetchColors } from "../redux/thunks/colorThunks";
+import Loader from "./Loader";
 
 const ColorList = () => {
   const dispatch: AppDispatch = useDispatch();
@@ -17,15 +18,19 @@ const ColorList = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [selectedColor, setSelectedColor] = useState<Color | null>(null);
 
-  const filteredColors = colors
-    .filter(
-      (colors) =>
-        colors.colorName
-          .toLowerCase()
-          .includes((searchQuery ?? "").toLowerCase()) ||
-        colors.hexCode.toLowerCase().includes((searchQuery ?? "").toLowerCase())
-    )
-    .reverse();
+  const filteredColors = useMemo(() => {
+    return colors
+      .filter(
+        (colors) =>
+          colors.colorName
+            .toLowerCase()
+            .includes((searchQuery ?? "").toLowerCase()) ||
+          colors.hexCode
+            .toLowerCase()
+            .includes((searchQuery ?? "").toLowerCase())
+      )
+      .reverse();
+  }, [colors, searchQuery]);
 
   useEffect(() => {
     dispatch(fetchColors());
@@ -42,29 +47,7 @@ const ColorList = () => {
   return (
     <div className="sm:min-w-[500px]  max-h-[500px] w-full overflow-y-scroll mx-auto p-6 border border-gray-300 rounded-lg inset-shadow">
       <div className="flex flex-wrap justify-start gap-4">
-        {loading && (
-          <div className="flex justify-center w-full">
-            <svg
-              className="w-32 h-32 text-blue-600 animate-spin"
-              viewBox="0 0 24 24"
-              fill="none"
-            >
-              <circle
-                className="opacity-25"
-                cx="12"
-                cy="12"
-                r="10"
-                stroke="currentColor"
-                strokeWidth="4"
-              ></circle>
-              <path
-                className="opacity-75"
-                fill="currentColor"
-                d="M4 12a8 8 0 018-8v8H4z"
-              ></path>
-            </svg>
-          </div>
-        )}
+        {loading && <Loader />}
         {error && <p className="text-red-500">{error}</p>}
         {!loading &&
           filteredColors.map((color) => (
---
+++ frontend/src/components/Loader.tsx
@@ -0,0 +1,27 @@
+const Loader = () => {
+  return (
+    <div className="flex justify-center w-full">
+      <svg
+        className="w-32 h-32 text-blue-600 animate-spin"
+        viewBox="0 0 24 24"
+        fill="none"
+      >
+        <circle
+          className="opacity-25"
+          cx="12"
+          cy="12"
+          r="10"
+          stroke="currentColor"
+          strokeWidth="4"
+        ></circle>
+        <path
+          className="opacity-75"
+          fill="currentColor"
+          d="M4 12a8 8 0 018-8v8H4z"
+        ></path>
+      </svg>
+    </div>
+  );
+};
+
+export default Loader;
